3c339aa726871518cda91d38aa4813d264123203
Kelsey Gilbert <jdashg@gmail.com>
2021-12-27 11:46:39 -0800

    Clean up build.py, change output to .scr.

--- build.py
+++ build.py
@@ -8,10 +8,20 @@ import subprocess
 DIR = pathlib.Path(__file__).parent
 OUT = DIR / 'out'
 
-if not OUT.exists():
-   os.mkdir(OUT)
+if OUT.exists():
+   shutil.rmtree(OUT)
+os.mkdir(OUT)
 os.chdir(OUT)
 
-# cl user32.lib advapi32.lib gdi32.lib klc-slideshow.cpp /link /subsystem:WINDOWS
-subprocess.run('cl /std:c++17 /EHsc ..\klc-slideshow.cpp /link /subsystem:WINDOWS', check=True);
+COMPILE_FLAGS = "/std:c++17 /EHsc".split(' ')
+FILES = "..\klc-slideshow.cpp".split(' ')
+LINK_FLAGS = "/link /subsystem:WINDOWS".split(' ')
+OUTPUT = "klc-slideshow.scr"
+
+def run(*args):
+   print('> ', args, '\n')
+   return subprocess.run(args, check=True);
+
+link_flags = LINK_FLAGS + ['/out:'+OUTPUT]
+run('cl', *COMPILE_FLAGS, *FILES, *link_flags);
 print('Build complete.')
